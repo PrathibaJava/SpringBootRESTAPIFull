f3efcdb918624e9696b12b56944baca260d38c97
Prathiba <javadeveloper.prathiba@gmail.com>
2023-05-21 05:25:46 +0100

    Initial commit

---
+++ src/main/java/com/restfulapi/fullenterprise/FullenterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.restfulapi.fullenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullenterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullenterpriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restfulapi/fullenterprise/FullenterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfulapi.fullenterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullenterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
