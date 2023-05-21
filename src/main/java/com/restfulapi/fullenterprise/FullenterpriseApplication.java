9f0d2b44aca28e817e420a242d6c8f6e88a9c227
Prathiba <javadeveloper.prathiba@gmail.com>
2023-05-21 05:39:50 +0100

    Second commit

--- src/main/java/com/restfulapi/fullenterprise/FullenterpriseApplication.java
+++ src/main/java/com/restfulapi/fullenterprise/FullenterpriseApplication.java
@@ -3,6 +3,7 @@ package com.restfulapi.fullenterprise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Adding new comment
 @SpringBootApplication
 public class FullenterpriseApplication {
 
